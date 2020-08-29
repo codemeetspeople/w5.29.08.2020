6e534cc27a9f42028ae97f6540be22db4a2a678e
caiman <me@caiman.dev>
2020-08-29 19:27:57 +0300

    [master] Reflection

---
+++ reflection.py
@@ -0,0 +1,34 @@
+class Integer:
+    def __init__(self, value):
+        self.value = int(value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def __add__(self, other):
+        if isinstance(other, Integer):
+            return Integer(self.value + other.value)
+        return NotImplemented
+
+
+class Float:
+    def __init__(self, value):
+        self.value = float(value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def __add__(self, other):
+        if isinstance(other, Float):
+            return Float(self.value + other.value)
+        return NotImplemented
+
+    def __radd__(self, other):
+        if isinstance(other, Integer):
+            return Integer(self.value + other.value)
+        return NotImplemented
+
+integer = Integer(5)
+float = Float(5)
+
+print(integer + float)
