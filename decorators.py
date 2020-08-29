7b02b3cc495e0b42c9207d04e385024610067f51
caiman <me@caiman.dev>
2020-08-29 17:53:39 +0300

    [master] Create decorator with own parameters

--- decorators.py
+++ decorators.py
@@ -1,20 +1,26 @@
-def run_5_times(func):
-    def wrapper(*args, **kwargs):
-        for _ in range(5):
-            func(*args, **kwargs)
-    
-    return wrapper
+def run_N_times(times):
+    def outer_wrapper(func):
+        def inner_wrapper(*args, **kwargs):
+            for _ in range(times):
+                func(*args, **kwargs)
+        
+        inner_wrapper.__name__ = func.__name__
+        return inner_wrapper
+    return outer_wrapper
 
 
-@run_5_times
+@run_N_times(3)
 def hello(username):
     print(f'Hello {username}!')
 
-@run_5_times
+@run_N_times(5)
 def point(x, y, color='black'):
     print(f'point({x}, {y}) is {color}')
 
 
-hello('caiman')
+print(hello.__name__)
+print(point.__name__)
 
-point(3, 5, 'blue')
+
+hello('caiman')
+point(3, 5, 'black')
